b9c2feb524cd09281d9cc6a79f9d2d1c8f163965
dip.ghosh <dip.ghosh@shohoz.com>
2022-06-18 01:34:18 +0600

    square root done

---
+++ Leet-code/easy/69. Sqrt(x).cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    int number;
+    cout << "Enter the number that you want to find the square root: ";
+    cin >> number;
+    cout << sqrt(number);
+    return 0;
+}
