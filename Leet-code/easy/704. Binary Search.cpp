7e12bc4eb181740564afbf9c4b00b69361473774
dip.ghosh <dip.ghosh@shohoz.com>
2022-06-21 12:20:41 +0600

    binary search done

---
+++ Leet-code/easy/704. Binary Search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+      int left = 0;
+      int right= sizeof(nums)/sizeof(nums[0]);
+      
+      while(left <= right){
+        
+        int mid = (left+right)/2;
+        
+        if(target == nums[mid]) return mid;
+        
+        else if(target > nums[mid])
+          left = mid + 1;
+        else
+          right = mid -1;
+      }
+      return -1;
+    }
+};
---
+++ Leet-code/easy/704. Binary Search.php
@@ -0,0 +1,29 @@
+class Solution {
+
+/**
+ * @param Integer[] $nums
+ * @param Integer $target
+ * @return Integer
+ */
+function search($nums, $target) {
+
+  $l = 0;
+  $r = count($nums);
+
+  while($l <= $r)
+  {
+    $m = (int)(($l+$r)/2);
+
+    if($target == $nums[$m]) return $m;
+
+    else if($target > $nums[$m]) $l = $m +1;
+
+    else  $r = $m -1;
+
+
+  }
+
+  return -1;
+
+}
+}
