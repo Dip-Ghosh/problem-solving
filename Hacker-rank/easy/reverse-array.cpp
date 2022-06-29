3923414a3001e574d9b7332a5530cefd0c18f6f0
dip.ghosh <dip.ghosh@shohoz.com>
2022-06-29 23:56:53 +0600

    reverse array problem solve

---
+++ Hacker-rank/easy/reverse-array.cpp
@@ -0,0 +1,24 @@
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }  
+    
+    for(int j=n-1; j>=0;j--)
+    {
+        cout<< arr[j] <<"\t" ;
+    }
+    return 0;
+}
