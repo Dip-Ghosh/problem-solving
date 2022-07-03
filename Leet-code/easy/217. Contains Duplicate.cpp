c157f955e3473d0986f7aaf3f3f5c520d7dc3b84
dip.ghosh <dip.ghosh@shohoz.com>
2022-07-03 18:55:20 +0600

    contains duplicate done

--- Leet-code/easy/217. Contains Duplicate.cpp
+++ Leet-code/easy/217. Contains Duplicate.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        
-      long size = nums.size();
       
-      int count = 0;
-      
-  
-      for( int i = 0; i < size; i++ )
+      // time complexity O(nlogn) as sort uses nlogn space complexity O(n)
+      sort(nums.begin(),nums.end());
+     
+      for( int i = 0; i < nums.size() -1; i++)
       {
-        for( int j = i+1; j < size; j++ )
-        {
-          if(nums[i] == nums[j]) count++;
-        }
-        
+        if(nums[i] == nums[i+1]) return true;
+       
       }
+       return false;
+    
+
+   // time complexity O(n) as sort uses nlogn space complexity O(n)
+     unordered_map<int,int> m;
       
-      if(count>0) return true;
+      for(auto a:nums) m[a]++;
+     
+      for(auto a:m)
+      {
+        if(a.second >1) return true;
+      }
+
+      return false;
+
+
+   // time complexity O(n) as sort uses space complexity O(n)
+        unordered_set<int> s;
+      for(auto a:nums) s.insert(a);
+      if(s.size() != nums.size()) return true;
       else return false;
-    }
+  
+
 };
