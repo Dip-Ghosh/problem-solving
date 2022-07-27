77fb7447deaecca413b484e3c6392e5570ead40e
Dip Ghosh <39679672+Dip-Ghosh@users.noreply.github.com>
2022-07-27 13:49:36 +0600

    Time: 49 ms (86.00%), Space: 20.6 MB (88.50%) - LeetHub

---
+++ 268-missing-number/268-missing-number.php
@@ -0,0 +1,20 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function missingNumber($nums) {
+       
+        $l = count($nums);
+        $sum = 0;
+        
+        for($i =0 ; $i< $l ;$i++){
+            $sum += $nums[$i];
+        }
+        $missingNumber = $l*($l+1)/2 -$sum;
+        
+        return $missingNumber;
+        
+    }
+}
