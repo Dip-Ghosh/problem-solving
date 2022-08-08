4da82b6bc9383b5f73f331ee2f3225b4f1d429e8
Dip Ghosh <39679672+Dip-Ghosh@users.noreply.github.com>
2022-08-08 16:41:39 +0600

    Time: 806 ms (13.70%), Space: 0B (100.00%) - LeetHub

--- 178-rank-scores/178-rank-scores.sql
+++ 178-rank-scores/178-rank-scores.sql
@@ -1,6 +1,13 @@
+# SELECT S.Score, COUNT(S2.Score) as `Rank`
+# FROM Scores S,
+# (SELECT DISTINCT Score FROM Scores) S2
+# WHERE S.Score<=S2.Score
+# GROUP BY S.Id
+# ORDER BY S.Score DESC;
 
-select Score,
-(select count(distinct Score) from Scores where Score >= s.Score)
-as Rank
+select s.score, count( distinct s2.score) as `rank`
 from Scores as s
-order by Score desc;
+join 
+Scores as s2 on s2.score >= s.score
+group by s.id
+order by s.score desc
